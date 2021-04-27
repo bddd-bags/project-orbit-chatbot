4090db6126a8626467c7c88381037feedbe7b32a
Josegalbraith <59763351+Josegalbraith@users.noreply.github.com>
2021-04-28 05:41:33 +0700

    update frontend

--- frontend/app.py
+++ frontend/app.py
@@ -7,10 +7,10 @@ app = Flask(__name__)
 def index():
      return render_template("index.html") 
      
-# @app.route("/get")
-# def get_bot_response():
-#      userText = request.args.get("msg") 
-#      return 
+@app.route("/get")
+def get_bot_response():
+     userText = request.args.get("msg") 
+     return str("ga tau, coba tanya mang ucup?")
 
 if __name__ == "__main__":
      app.run(debug = True)
