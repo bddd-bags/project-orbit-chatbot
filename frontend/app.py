1732cef5def5d64eb9e74e74141273b0f5482aa6
Josegalbraith <59763351+Josegalbraith@users.noreply.github.com>
2021-04-27 07:29:08 +0700

    Add files via upload

---
+++ frontend/app.py
@@ -0,0 +1,18 @@
+from flask import Flask,render_template,request
+app = Flask(__name__) 
+
+
+
+@app.route("/")
+def index():
+     return render_template("index.html") 
+     
+# @app.route("/get")
+# def get_bot_response():
+#      userText = request.args.get("msg") 
+#      return 
+
+if __name__ == "__main__":
+     app.run(debug = True)
+
+
